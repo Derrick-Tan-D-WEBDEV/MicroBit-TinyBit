ffa1f111a9e82012f154463e3bf2dd9a6c98924b
Derrick Tan D-WEBDEV <derricktpl@derrickwebdev.com>
2021-03-31 16:04:45 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+let distance = 0
+Tinybit.RGB_Car_Big(Tinybit.enColor.Cyan)
+basic.forever(function () {
+    distance = Tinybit.Ultrasonic_Car()
+    if (distance < 15) {
+        Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Back, 60)
+        basic.pause(200)
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinRight, 60)
+        basic.pause(200)
+    } else {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 80)
+    }
+})
