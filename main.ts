dcda47341d14b539ab731cdabd28bd0242d6d798
Derrick Tan D-WEBDEV <derricktpl@derrickwebdev.com>
2021-03-31 18:25:21 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,13 @@
-let distance = 0
 Tinybit.RGB_Car_Program().showColor(neopixel.colors(NeoPixelColors.Orange))
 Tinybit.RGB_Car_Big(Tinybit.enColor.Cyan)
 basic.forever(function () {
-    distance = Tinybit.Ultrasonic_Car()
-    if (distance < 15) {
-        Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Back, 60)
-        basic.pause(200)
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinRight, 60)
-        basic.pause(200)
-    } else {
+    if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
         Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 80)
+    } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinLeft, 80)
+    } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinRight, 80)
+    } else {
+        Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
     }
 })
